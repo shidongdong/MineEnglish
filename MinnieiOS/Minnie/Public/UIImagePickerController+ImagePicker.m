e6b92cf6b45496ff88e72e2fca72871b292853fd
songzhen <1933343046@qq.com>
2019-06-13 15:01:15 +0800

    OCT ipad管理端只支持横屏调用系统相册，弹出方式为竖屏问题

---
+++ MinnieiOS/Minnie/Public/UIImagePickerController+ImagePicker.m
@@ -0,0 +1,29 @@
+//
+//  UIImagePickerController+ImagePicker.m
+//  Minnie
+//
+//  Created by songzhen on 2019/6/13.
+//  Copyright © 2019 minnieedu. All rights reserved.
+//
+
+#import "UIImagePickerController+ImagePicker.h"
+
+@implementation UIImagePickerController (ImagePicker)
+
+#if MANAGERSIDE
+
+- (BOOL)shouldAutorotate{
+    return YES;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations{
+    
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+//- (NSUInteger)supportedInterfaceOrientations{
+//    return UIInterfaceOrientationMaskLandscape;
+//}
+
+#endif
+@end
