48527eea824f43774cdecaf6e535fc22fd17552e
songzhen <1933343046@qq.com>
2019-08-21 17:07:57 +0800

    OCT 试听课

--- MinnieiOS/Minnie/TrialClass/Service/Request/TrialClasssRequest.h
+++ MinnieiOS/Minnie/TrialClass/Service/Request/TrialClasssRequest.h
@@ -8,6 +8,17 @@
 
 #import "BaseRequest.h"
 
+#pragma mark - 2.5.1    获取班级列表（学生端，教师端，ipad端）
 @interface TrialClasssRequest : BaseRequest
+
+@end
+
+
+#pragma mark - 2.5.7    试听课报名（学生端）
+@interface EnrollTrialClassRequest : BaseRequest
+
+- (instancetype)initWithName:(NSString *)name
+                       grade:(NSString *)grade
+                      gender:(NSInteger)gender;
 
 @end
--- MinnieiOS/Minnie/TrialClass/Service/Request/TrialClasssRequest.m
+++ MinnieiOS/Minnie/TrialClass/Service/Request/TrialClasssRequest.m
@@ -8,6 +8,7 @@
 
 #import "TrialClasssRequest.h"
 
+#pragma mark - 2.5.1    获取班级列表（学生端，教师端，ipad端）
 @implementation TrialClasssRequest
 
 - (YTKRequestMethod)requestMethod {
@@ -23,3 +24,42 @@
 }
 
 @end
+
+
+#pragma mark - 2.5.7    试听课报名（学生端）
+@interface EnrollTrialClassRequest()
+
+@property (nonatomic, assign) NSInteger classId;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *grade;
+@property (nonatomic, assign) NSInteger gender;
+
+@end
+
+@implementation EnrollTrialClassRequest
+
+- (instancetype)initWithName:(NSString *)name
+                       grade:(NSString *)grade
+                      gender:(NSInteger)gender {
+    self = [super init];
+    if (self != nil) {
+        _name = name;
+        _grade = grade;
+        _gender = gender;
+    }
+    
+    return self;
+}
+
+- (NSString *)requestUrl {
+    return [NSString stringWithFormat:@"%@/class/enroll", ServerProjectName];
+}
+
+- (id)requestArgument {
+    return @{@"nickname":self.name,
+             @"grade":self.grade,
+             @"gender":@(self.gender)};
+}
+
+@end
+
